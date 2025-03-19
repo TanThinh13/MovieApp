1cf4913f4e328c185bd0990604fbd5e1bf952a29
TanThinh13 <6251071097@st.utc2.edu.vn>
2025-03-19 11:00:27 +0700

    Fix

--- data/supbaseClient.ts
+++ data/supbaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://lzlycyjvsglfjkuxltqx.supabase.co"; 
+
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6bHljeWp2c2dsZmprdXhsdHF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5NjE0NjcsImV4cCI6MjA1NzUzNzQ2N30.MtqgAX_fbNAvJ5YnE5nl1_C8l6VozK0QOP5jTW2Bg4c"; 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
